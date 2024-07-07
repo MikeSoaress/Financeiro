97b06f4a3563943f8ffb5e5258a5891d19e35f5d
Mike <mikesoares8@gmail.com>
2024-07-07 14:42:17 -0300

    ajuste

--- Financeiro.API/Program.cs
+++ Financeiro.API/Program.cs
@@ -13,11 +13,14 @@ builder.Services.AddInfrastructureAPI(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
